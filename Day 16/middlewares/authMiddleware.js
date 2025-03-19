1f5fdee40ccf0f5d1235305be48c433b31802f62
dziwGH <adziwanowski@atu.edu>
2025-03-19 13:31:24 -0500

    Added middlewares

---
+++ Day 16/middlewares/authMiddleware.js
@@ -0,0 +1,8 @@
+const authMiddleware = (req,res,next) => {
+    const token = req.headers['authorization'];
+    if(token === 'secret123') {
+        next(); //proceed
+    } else {
+        res.status(401).send({error: 'Unauthorized access'});
+    }
+};
